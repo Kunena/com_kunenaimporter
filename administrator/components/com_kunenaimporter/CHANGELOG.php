8c94f372ebf70aa198a61d53dc63d9a38de23465
mahagr <matias@kunena.org>
2010-04-02 06:28:00 +0000

    ^ [#20086] Move KunenaImporter to /trunk/1.6-addons
    git-svn-id: http://joomlacode.org/svn/kunena/trunk/1.6-addons/com_kunenaimporter@2148 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ administrator/components/com_kunenaimporter/CHANGELOG.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* @version $Id:$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no access
+die('Restricted access');
+?>
+<!--
+
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the Kunena Importer, including beta and release candidate versions.
+
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+30-Aug-2009 Matias
+# [#17875] Fix all errors and warnings found in Eclipse
+
+4-Aug-2009 Matias
++ [#17485] Initial version of Kunena Importer
+
+ -->
