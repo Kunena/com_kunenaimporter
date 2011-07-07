7c2ce5812d52388f20d22266cdc82c325accb741
Matias Griese <matias@kunena.org>
2011-07-07 21:07:13 +0300

    + [#5] phpBB3 support (oops, it was issue #5)

--- administrator/components/com_kunenaimporter/CHANGELOG.php
+++ administrator/components/com_kunenaimporter/CHANGELOG.php
@@ -33,11 +33,11 @@ Legend:
 
 KunenaImporter 1.6.5-DEV
 
-6-July-2011 Matias
-+ [#3] phpBB3 support: migrate uploaded avatars and gallery
+7-July-2011 Matias
++ [#5] phpBB3 support: migrate uploaded avatars and gallery
 
 6-July-2011 Matias
-+ [#3] phpBB3 support: migrate attachments
++ [#5] phpBB3 support: migrate attachments
 
 5-July-2011 Matias
 ^ [#1] Convert component to PHP5
