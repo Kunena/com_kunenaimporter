3fb2d76302efe154da755074f507ac0731e3edc1
Oliver Ratzesberger <oliver@kunena.org>
2011-07-08 23:46:52 -0700

    Recommit changelog: + [#12] Add basic version check for Kunena and disable if not present

--- administrator/components/com_kunenaimporter/CHANGELOG.php
+++ administrator/components/com_kunenaimporter/CHANGELOG.php
@@ -38,6 +38,7 @@ KunenaImporter 1.6.5
 # [#3] Update builder for new README.md
 ^ [#11] Update version info to 1.6.5
 + [#13] Add new flag to user migration table
++ [#12] Add basic version check for Kunena and disable if not present
 # [#14] Missing forum table prefix re-added
 
 5-July-2011 Matias
