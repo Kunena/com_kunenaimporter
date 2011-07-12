331709ac8b19bbc074e60f3e1bf666b44a269612
Matias Griese <matias@kunena.org>
2011-07-12 16:31:55 +0300

    [#30] Joomla exporters: remove functions that are now in parent

--- administrator/components/com_kunenaimporter/CHANGELOG.php
+++ administrator/components/com_kunenaimporter/CHANGELOG.php
@@ -38,6 +38,7 @@ KunenaImporter 1.6.5-DEV
 - [#30] KunenaimporterModelExport: remove the need of buildImportOps()
 ^ [#30] Move all message generation to KunenaimporterModelExport::detect()
 - [#30] Example exporter: remove function buildImportOps()
+- [#30] All Joomla exporters: remove functions that are now defined in parent class
 
 11-July-2011 Matias
 + [#30] Add example exporter which can be used to create new exporters
