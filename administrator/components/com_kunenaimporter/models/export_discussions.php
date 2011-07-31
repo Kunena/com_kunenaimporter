5183a862cdf63bf428332dffe5076c085c5457a7
Florian DAL FITTO <xillibit@gmail.com>
2011-07-31 15:39:26 +0200

    + [#42] Configuration options import for Joo!BB + [#25] Configuration options import for NinjaBoard + [#44] Add suuport for Discussions

--- administrator/components/com_kunenaimporter/models/export_discussions.php
+++ administrator/components/com_kunenaimporter/models/export_discussions.php
@@ -13,87 +13,58 @@ defined ( '_JEXEC' ) or die ();
 
 require_once( JPATH_COMPONENT . '/models/export.php' );
 
-class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
+class KunenaimporterModelExport_Discussions extends KunenaimporterModelExport {
 	/**
 	 * Extension name ([a-z0-9_], wihtout 'com_' prefix)
 	 * @var string
 	 */
-	public $name = 'ninjaboard';
+	public $name = 'discussions';
 	/**
 	 * Display name
 	 * @var string
 	 */
-	public $title = 'NinjaBoard';
+	public $title = 'Discussions';
 	/**
 	 * Minimum required version
 	 * @var string or null
 	 */
-	protected $versionmin = '1.0';
+	protected $versionmin = '1.3';
 	/**
 	 * Maximum accepted version
 	 * @var string or null
 	 */
 	protected $versionmax = null;
 
-	public function countConfig() {
-		return 1;
-	}
-
 	/**
-	 * Get NinjaBoard version
+	 * Get Discussions version
 	 */
 	public function getVersion() {
-		// NinjaBoard version can be found from manifest.xml file
-		$xml = JPATH_ADMINISTRATOR . "/components/com_{$this->name}/manifest.xml";
-		if (!JFile::exists ( $xml )) {
-			return false;
-		}
-		$parser = JFactory::getXMLParser ( 'Simple' );
-		$parser->loadFile ( $xml );
-		return $parser->document->getElementByPath ( 'version' )->data ();
+		$query = "SELECT version FROM `#__discussions_meta` WHERE id='1'";
+		$this->ext_database->setQuery ( $query );
+		return $this->ext_database->loadResult ();
 	}
 
-	/**
-	 * Get Ninkaboard configuration
-	 */
-	public function &getConfig() {
-		if (empty($this->config)) {
-			$query = "SELECT params FROM `#__ninjaboard_settings` WHERE enabled = '1'";
-			$this->ext_database->setQuery ( $query );
-			$config = $this->ext_database->loadResult ();
-
-			$config = json_decode($config, true);
-
-			$config_options = new stdclass();
-
-			foreach($config as $value) {
-				foreach($value as $key=>$opt) {
-					$config_options->$key=$opt;
-				}
-			}
-
-			if ( !empty($config_options) ) $this->config = $config_options;
-			else $this->config = '';
-		}
-		return $this->config;
+	public function countConfig() {
+		return 1;
 	}
 
 	public function &exportConfig($start=0, $limit=0) {
 		$config = array();
 		if ($start) return $config;
 
-		$NinjaBoardConfig = $this->getConfig ();
+		// FIX ME: the default config values are stored in JPATH_ADMINISTRATOR . "/components/com_{$this->name}/config.xml";
+		// but when you change these value i don't where the data are stored, there is no table in database for that !
 
 		$config['id'] = 1;
-		// $config['board_title'] = null;
-		// $config['email'] = null;
+		// $config['board_title'] = null; --> Sitename
+		// $config['email'] = null; --> from
 		// $config['board_offline'] = null;
 		// $config['board_ofset'] = null;
 		// $config['offline_message'] = null;
-		// $config['enablerss'] = null;
+		// $config['enablerss'] = null; -->Use RSS Feeds
 		// $config['enablepdf'] = null;
-		// $config['threads_per_page'] = null;
-		// $config['messages_per_page'] = null;
+		// $config['threads_per_page'] = null; --> # of threads in category list
+		// $config['messages_per_page'] = null; --> # of posts in thread list
 		// $config['messages_per_page_search'] = null;
 		// $config['showhistory'] = null;
 		// $config['historylimit'] = null;
@@ -110,12 +81,12 @@ class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
 		// $config['rteheight'] = null;
 		// $config['enableforumjump'] = null;
 		// $config['reportmsg'] = null;
-		$config['username'] = $NinjaBoardConfig->display_name=='username' ? 1 : 0;
+		// $config['username'] = null;
 		// $config['askemail'] = null;
 		// $config['showemail'] = null;
 		// $config['showuserstats'] = null;
 		// $config['showkarma'] = null;
-		// $config['useredit'] = null;
+		// $config['useredit'] = null; -->
 		// $config['useredittime'] = null;
 		// $config['useredittimegrace'] = null;
 		// $config['editmarkup'] = null;
@@ -137,13 +108,13 @@ class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
 		// $config['allowavatargallery'] = null;
 		// $config['avatarquality'] = null;
 		// $config['avatarsize'] = null;
-		$config['allowimageupload'] = $NinjaBoardConfig->enable_attachments;
+		// $config['allowimageupload'] = null; --> Images allowed for
 		// $config['allowimageregupload'] = null;
 		// $config['imageheight'] = null;
 		// $config['imagewidth'] = null;
-		// $config['imagesize'] = null;
+		// $config['imagesize'] = null; --> Max Image Size
 		// $config['allowfileupload'] = null;
-		$config['allowfileregupload'] = $NinjaBoardConfig->enable_attachments;
+		// $config['allowfileregupload'] = null;
 		// $config['filetypes'] = null;
 		// $config['filesize'] = null;
 		// $config['showranking'] = null;
@@ -171,7 +142,7 @@ class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
 		// $config['popusercount'] = null;
 		// $config['showpopsubjectstats'] = null;
 		// $config['popsubjectcount'] = null;
-		$config['usernamechange'] = $NinjaBoardConfig->change_display_name=='custom' ? 1 : 0;
+		// $config['usernamechange'] = null;
 		// $config['rules_infb'] = null;
 		// $config['rules_cid'] = null;
 		// $config['help_infb'] = null;
@@ -262,134 +233,151 @@ class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
 		// $config['pubprofile'] = null;
 		// $config['thankyou_max'] = null;
 		$result = array('1'=>$config);
-
 		return $result;
 
 	}
 
 	public function countCategories() {
-		$query="SELECT COUNT(*) FROM #__ninjaboard_forums";
+		$query="SELECT COUNT(*) FROM #__discussions_categories";
 		$count = $this->getCount($query);
 		return $count;
 	}
 
-	public function &exportCategories($start=0, $limit=0) {
-		// Import the forums - Ninjaboard does not store separate categories
-		$query="(SELECT
-			ninjaboard_forum_id AS id,
-			enabled AS published,
-			title AS name,
+	/**
+	 * Export sections and categories
+	 *
+	 * Returns list of category objects containing database fields
+	 * to #__kunena_categories.
+	 * All categories without parent are sections.
+	 *
+	 * NOTE: it's very important to keep category IDs (containing topics) the same!
+	 * If there are two tables for sections and categories, change IDs on sections..
+	 *
+	 * @param int $start Pagination start
+	 * @param int $limit Pagination limit
+	 * @return array
+	 */
+	public function &exportCategories($start = 0, $limit = 0) {
+		$query = "SELECT
+			id AS id,
+			parent_id AS parent,
+			name AS name,
+			0 AS cat_emoticon,
+			0 AS locked,
+			moderated AS moderated,
+			0 AS pub_access,
+			1 AS pub_recurse,
+			0 AS admin_access,
+			1 AS admin_recurse,
+			ordering AS ordering,
+			published AS published,
+			0 AS review,
+			0 AS allow_anonymous,
+			0 AS post_anonymous,
+			0 AS hits,
 			description AS description,
-			/* forum_mdesc AS headerdesc, TODO: extract description ouf of param */
-			topics AS numTopics,
-			ordering,
-			posts AS numPosts,
-			last_post_id AS id_last_msg,
-			ninjaboard_forum_id AS id,
-			path AS parent
-		FROM #__ninjaboard_forums)
+			'' AS headerdesc,
+			'' AS class_sfx,
+			0 AS allow_polls,
+			last_entry_user_id AS id_last_msg,
+			counter_posts AS numPosts,
+			counter_threads AS numTopics,
+			last_entry_date AS time_last_msg
+		FROM #__discussions_categories
 		ORDER BY id";
-		$result = $this->getExportData($query, $start, $limit);
-		foreach ($result as $key=>&$row) {
-			if( $row->parent=='/' ) {
-				$row->parent='0';
-			} else {
-				$row->parent=preg_replace('#/?#','',$row->parent);
-			}
-			$row->name = $this->prep($row->name);
-			$row->description = $this->prep($row->description);
-		}
-		return $result;
-	}
-
-	public function countMessages() {
-		$query = "SELECT COUNT(*) FROM #__ninjaboard_messages";
-		return $this->getCount ( $query );
-	}
-
-	public function &exportMessages($start = 0, $limit = 0) {
-		// TODO: in replies the subject is empty, need to find a way to fill it
- 		$query = "SELECT
- 			p.ninjaboard_post_id AS id,
-			IF(p.ninjaboard_post_id=t.ninjaboard_topic_id,0,t.ninjaboard_topic_id) AS parent,
-			t.first_post_id AS thread,
-			t.forum_id AS catid,
-			u.username AS name,
-			p.subject,
-			p.text AS message,
-			UNIX_TIMESTAMP(p.created_time) AS time,
-			p.created_user_id AS userid,
-			p.modified_user_id AS modified_by,
-			UNIX_TIMESTAMP(p.modified) AS modified_time,
-			p.edit_reason AS modified_reason,
-			p.user_ip AS ip,
-			p.locked,
-			p.ninjaboard_topic_id AS topic_emoticon
-			FROM #__ninjaboard_posts AS p
-			LEFT JOIN #__ninjaboard_topics AS t ON t.ninjaboard_topic_id=p.ninjaboard_topic_id
-			LEFT JOIN #__users AS u ON u.id=p.created_user_id";
 		$result = $this->getExportData ( $query, $start, $limit, 'id' );
-		foreach ( $result as &$row ) {
-			$row->subject = $this->prep ( $row->subject );
-			$row->message = $this->prep ( $row->message );
+		foreach ( $result as $key => &$row ) {
+			$this->parseText ( $row->name );
+			$this->parseBBCode ( $row->description );
+			$this->parseBBCode ( $row->headerdesc );
+			$this->parseText ( $row->class_sfx );
 		}
 		return $result;
 	}
 
-	/* TODO: CHeck if smilies and ranks are available outside of db
-	public function countSmilies() {
-		return false;
-
-		$query="SELECT COUNT(*) FROM #__agora_smilies";
-		return $this->getCount($query);
+	/**
+	 * Count total number of messages to be exported
+	 */
+	public function countMessages() {
+		$query = "SELECT COUNT(*) FROM #__discussions_messages";
+		$count = $this->getCount ( $query );
+		return $count;
 	}
 
-	public function &exportSmilies($start=0, $limit=0)
-	{
-		$query="SELECT image AS location, text FROM #__agora_smilies";
-		$result = $this->getExportData($query, $start, $limit);
-		return $result;
-	}*/
-
-	public function countRanks() {
-		return false;
-
-		$query="SELECT COUNT(*) FROM #__ninjaboard_ranks";
-		return $this->getCount($query);
-	}
-
-	public function &exportRanks($start=0, $limit=0)
-	{
-		$query="SELECT
-			title AS rank_title,
-			min AS rank_min,
-			rank_file AS rank_image
-		FROM #__ninjaboard_ranks";
-		$result = $this->getExportData($query, $start, $limit);
-		foreach ( $result as $rank ) {
-			$this->parseText ( $row->rank_title );
-			// Full path to the original file
-			$rank->copyfile = JPATH_ROOT . "/media/com_ninjaboard/images/rank/{$rank->rank_image}";
+	/**
+	 * Export messages
+	 *
+	 * Returns list of message objects containing database fields
+	 * to #__kunena_messages (and #__kunena_messages_text.message).
+	 *
+	 * @param int $start Pagination start
+	 * @param int $limit Pagination limit
+	 * @return array
+	 */
+	public function &exportMessages($start = 0, $limit = 0) {
+		$query = "SELECT
+			id AS id,
+			parent_id AS parent,
+			thread AS thread,
+			cat_id AS catid,
+			account AS name,
+			user_id AS userid,
+			'' AS email,
+			subject AS subject,
+			UNIX_TIMESTAMP(date) AS time,
+			ip AS ip,
+			0 AS topic_emoticon,
+			locked AS locked,
+			0 AS hold,
+			0 AS ordering,
+			hits AS hits,
+			0 AS moved,
+			last_entry_user_id AS modified_by,
+			UNIX_TIMESTAMP(last_entry_date) AS modified_time,
+			'' AS modified_reason,
+			message AS message
+		FROM #__discussions_messages";
+		$result = $this->getExportData ( $query, $start, $limit );
+		foreach ( $result as $key => &$row ) {
+			$this->parseText ( $row->name );
+			$this->parseText ( $row->email );
+			$this->parseText ( $row->subject );
+			$this->parseText ( $row->modified_reason );
+			$this->parseBBCode ( $row->message );
 		}
 		return $result;
 	}
 
-	public function countUserprofile() {
-		$query="SELECT COUNT(*) FROM #__ninjaboard_people";
-		return $this->getCount($query);
+	/**
+	 * Count total number of user profiles to be exported
+	 */
+	public function countUserProfile() {
+		$query = "SELECT COUNT(*) FROM #__discussions_users";
+		$count = $this->getCount ( $query );
+		return $count;
 	}
 
-	public function &exportUserprofile($start=0, $limit=0) {
-		$query="SELECT
-			ninjaboard_person_id AS userid,
-			signature,
-			posts AS posts,
-			avatar,
+	/**
+	 * Export user profiles
+	 *
+	 * Returns list of user profile objects containing database fields
+	 * to #__kunena_users.
+	 * NOTE: copies all files found in $row->copyfile (full path) to Kunena.
+	 *
+	 * @param int $start Pagination start
+	 * @param int $limit Pagination limit
+	 * @return array
+	 */
+	public function &exportUserProfile($start = 0, $limit = 0) {
+		$query = "SELECT
+			id AS userid,
 			'flat' AS view,
-			'' AS signature,
-			0 AS moderator,
+			signature AS signature,
+			moderator AS moderator,
 			NULL AS banned,
 			0 AS ordering,
+			posts AS posts,
+			'' AS avatar,
 			0 AS karma,
 			0 AS karma_time,
 			0 AS uhits,
@@ -402,8 +390,8 @@ class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
 			NULL AS YIM,
 			NULL AS MSN,
 			NULL AS SKYPE,
-			NULL AS TWITTER,
-			NULL AS FACEBOOK,
+			twitter AS TWITTER,
+			facebook AS FACEBOOK,
 			NULL AS GTALK,
 			NULL AS MYSPACE,
 			NULL AS LINKEDIN,
@@ -411,76 +399,26 @@ class KunenaimporterModelExport_Ninjaboard extends KunenaimporterModelExport {
 			NULL AS FRIENDFEED,
 			NULL AS DIGG,
 			NULL AS BLOGSPOT,
-			NULL AS FLICKR,
+			flickr AS FLICKR,
 			NULL AS BEBO,
 			NULL AS websitename,
-			NULL AS websiteurl,
+			website AS websiteurl,
 			NULL AS rank,
 			0 AS hideEmail,
-			1 AS showOnline
-		FROM #__ninjaboard_people";
-		$result = $this->getExportData($query, $start, $limit);
-		foreach ( $result as $key => &$row ) {
-		  if ( !empty($row->avatar) ) $row->copypath = JPATH_ROOT . $row->avatar;
-		}
+			show_online_status AS showOnline
+		FROM #__discussions_users
+		ORDER BY userid";
+		$result = $this->getExportData ( $query, $start, $limit, 'userid' );
+
+		/*foreach ( $result as &$row ) {
+			if ($row->avatar) {
+				// Full path to the original file
+				$row->copyfile = JPATH_ROOT . "/media/com_example/avatars/{$row->avatar}";
+			}
+			$this->parseBBCode ( $row->signature );
+			$this->parseText ( $row->personalText );
+			// Parse also all social data
+		}*/
 		return $result;
-	}
-
-	public function countSubscriptions() {
-		$query = "SELECT COUNT(*) FROM `#__ninjaboard_subscriptions`
-					WHERE subscription_type=3";
-		return $this->getCount ( $query );
-	}
-
-	public function &exportSubscriptions($start = 0, $limit = 0) {
-		$query = "SELECT
-			subscription_type_id AS thread,
-			created_by AS userid
-		FROM `#__ninjaboard_subscriptions`
-		WHERE subscription_type=3";
-		$result = $this->getExportData ( $query, $start, $limit );
-		return $result;
-	}
-
-	/**
-	 * Count total number of attachments to be exported
-	 */
-	public function countAttachments() {
-		$query = "SELECT COUNT(*) FROM #__ninjaboard_attachments";
-		$count = $this->getCount ( $query );
-		return $count;
-	}
-
-	/**
-	 * Export attachments in messages
-	 *
-	 *
-	 * @param int $start Pagination start
-	 * @param int $limit Pagination limit
-	 * @return array
-	 */
-	public function &exportAttachments($start = 0, $limit = 0) {
-		$query = "SELECT
-			ninjaboard_attachment_id AS id,
-			file AS filname,
-			joomla_user_id AS userid,
-			post_id AS mesid,
-			NULL AS hash,
-			NULL AS size,
-			NULL AS folder,
-			NULL AS filetype
-		FROM #__ninjaboard_attachments
-		ORDER BY id";
-		$result = $this->getExportData ( $query, $start, $limit, 'id' );
-		$copypath = JPATH_ROOT.'/media/com_ninjaboard/attachments';
-		foreach ( $result as &$row ) {
-			// Full path to the original file
-			$row->copyfile = "{$copypath}/{$row->filename}";
-		}
-		return $result;
-	}
-
-	protected function prep($s) {
-		return $s;
 	}
 }
